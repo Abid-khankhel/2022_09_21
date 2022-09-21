a34a0d6c12688311f9cede27d07e6931b350262c
israeel <israeel.abid@gmail.com>
2022-09-21 11:54:01 +0430

    first commit

---
+++ test.js
@@ -0,0 +1,101 @@
+// $('p').hide()
+
+
+// $(document).ready(function(){
+
+
+// $('button').mouseenter(function(){
+
+
+
+
+//     $('p').hide();
+
+// alert('paraghraph is hidded')
+
+
+
+// })
+// $('small').mouseenter(function(){
+
+// $('p').show()
+    
+// })
+
+
+
+// })
+
+
+
+$(document).ready(function(){
+// $('#header').hover(function(){
+
+
+// alert('you have intered a values')
+
+// })
+
+// $('button').mouseenter(function(){
+
+
+
+
+//     $('#p1').hide();
+
+// alert('you entered p1!')
+
+
+
+// })
+
+
+// $('button').click(function(){
+
+
+
+
+//     $('#p2').hide();
+
+// alert('action goes hare')
+
+
+
+// })
+
+// $('button').mouseleave(function(){
+
+
+
+
+//     $('#p3').hide();
+
+// alert('bye! you now leave p3')
+
+
+
+// })
+
+// $('button').dblclick(function(){
+
+
+
+
+//     $('#p4').hide();
+
+// alert('paraghraph is hidded')
+
+
+
+// })
+
+
+
+
+
+
+
+
+})
+
+
